def6de2059ba1c2a7ebc41fcdc4f70952748e724
vedantpawar18 <vedantpawar18@gmail.com>
2022-05-02 12:29:15 +0530

    a

---
+++ components/navbar.js
@@ -0,0 +1,8 @@
+function navbar (){
+
+    return `
+    <h1><a href="index.html">News App</a></h1>
+    <input type="text" id="search_input" placeholder="   Search News">`;
+}
+
+export default navbar;
---
+++ scripts/index.js
@@ -0,0 +1,103 @@
+// Ude Import export (MANDATORY)
+import navbar from "../components/navbar.js"
+document.getElementById("navbar").innerHTML=navbar();
+// Onclicking store the news in local storage with key "news" so that you can access that on news.html page
+
+
+var input=document.getElementById("search_input");
+
+input.addEventListener("keydown", function(e){
+    if(e.key=="Enter"){
+       
+       
+        validate(e);
+    }
+});
+
+
+var news_array=JSON.parse(localStorage.getItem("news")) || [];
+
+
+async function validate(e){
+
+    try {
+        let query=e.target.value;
+
+   let res=await fetch(`https://masai-mock-api.herokuapp.com/news?q=${query}`);
+
+   const data=await res.json();
+
+   const data1=data.articles;
+   getNews(data1)
+
+   console.log(data.articles);
+    }
+    catch(err){
+    console.log(err)
+}
+}
+
+
+function getNews(data1){
+
+    data1.forEach(function(el){
+       
+        news_array.push(el);
+        localStorage.setItem("news",JSON.stringify(news_array));
+         window.location.href="news.html"
+    });
+}
+
+
+async function homeData(){
+
+    try {
+        // let query="in";
+
+   let res=await fetch(`https://masai-mock-api.herokuapp.com/news/top-headlines?country=in`);
+
+   const data=await res.json();
+
+   const data1=data.articles;
+   appendHome(data1)
+   console.log(data1);
+    }
+    catch(err){
+    console.log(err)
+}
+}
+
+homeData()
+
+function appendHome(data1)
+{
+    
+    data1.forEach(function(el){
+
+        var div1=document.createElement("div");
+        div1.id="div1"
+        var div2=document.createElement("div");
+        div2.id="div2"
+        var div3=document.createElement("div");
+        div3.id="div3"
+
+        var img=document.createElement("img");
+        img.src=el.urlToImage;
+
+        var h4=document.createElement("h4");
+        h4.innerText=el.content;
+
+        var p=document.createElement("p");
+        p.innerText=el.description;
+
+        
+        div2.append(img)
+        div3.append(h4,p)
+        div1.append(div2,div3)
+
+        let box1=document.getElementById("results");
+
+        box1.append(div1)
+    
+    })
+}
